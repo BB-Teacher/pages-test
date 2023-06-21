77f0ada2c3b8822f3be20f526a404ab027187118
BB-Teacher <137305065+BB-Teacher@users.noreply.github.com>
2023-06-21 09:06:56 -0500

    practice

---
+++ code.js
@@ -0,0 +1,7 @@
+onEvent("leftButton", "click", function() {
+  setScreen("leftScreen");
+  playSound("assets/default.mp3", false);
+});
+onEvent("righbutton", "click", function(){
+setScreen("rightscreen");
+});
